b73e863e53f53f916c86d69158869cbd97cb7d77
Mateusz Kopówka <mkopowka@hotmail.com>
2022-11-21 11:56:51 +0100

    fix

--- Client/src/app/app.component.spec.ts
+++ Client/src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -32,4 +33,5 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.content span')?.textContent).toContain('Client app is running!');
   });
+
 });
