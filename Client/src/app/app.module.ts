583493c8eb22d820b4519f38b15e40a6f8b9d455
Mateusz Kopówka <mkopowka@hotmail.com>
2022-11-24 22:40:53 +0100

    routing

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
 import { ShopModule } from './shop/shop.module';
 
 @NgModule({
@@ -17,7 +18,7 @@ import { ShopModule } from './shop/shop.module';
     BrowserAnimationsModule,
     HttpClientModule,
     CoreModule,
-    ShopModule
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
